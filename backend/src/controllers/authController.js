14ef7b31d86bbc0d9b9c33f1eb812ba60987c19d
Himanshu <ht5620085@gmail.com>
2026-02-12 19:47:19 +0530

    feat (backend): adding auth controller and routes

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+const generateToken = require("../utils/generateToken")
+
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ msg: "User already exists. Please Sign In." });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({ username, email, password: hashedPassword });
+
+        return res.status(201).json({ id: user._id });
+    } catch (error) {
+        return res.status(500).json({ err: error.message });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select("+password");
+
+        if (!user) {
+            return res.status(400).json({ msg: "Invalid creds" });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            return res.status(400).json({ msg: "Invalid creds" });
+        }
+
+        const token = generateToken(user._id);
+    } catch (error) {
+        res.status(500).json({ err: error.message });
+    }
+}
