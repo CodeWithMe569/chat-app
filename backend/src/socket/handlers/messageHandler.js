18e4d5c16616606c264614e82eacde3414e2edd1
Himanshu <ht5620085@gmail.com>
2026-02-13 15:09:29 +0530

    feat (socket): saving message before broadcasting to room

--- backend/src/socket/handlers/messageHandler.js
+++ backend/src/socket/handlers/messageHandler.js
@@ -1,8 +1,24 @@
+const Message = require("../../models/Message")
+
 module.exports = (io, socket) => {
 
-  socket.on("send_message", ({ room, message }) => {
+  socket.on("send_message", async ({ room, content }) => {
 
-    io.to(room).emit("receive_message", message)
+    try {
+
+      // Save to DB
+      const message = await Message.create({
+        sender: socket.user.id,
+        room,
+        content
+      })
+
+      // Broadcast saved message
+      io.to(room).emit("receive_message", message)
+
+    } catch (err) {
+      console.error(err)
+    }
 
   })
 
