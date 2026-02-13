9dc4e0977ce19ea8275b0bb0d6557bd99ba005c2
Himanshu <ht5620085@gmail.com>
2026-02-13 15:14:28 +0530

    feat (socket): adding room validation so that no one cannot send message in any room

--- backend/src/socket/handlers/roomHandler.js
+++ backend/src/socket/handlers/roomHandler.js
@@ -1,11 +1,41 @@
+const Room = require("../../models/Room")
+
 module.exports = (socket) => {
 
-  socket.on("join_room", (roomId) => {
-    socket.join(roomId)
+  socket.on("join_room", async (roomId) => {
 
-    console.log(
-      `Socket ${socket.id} joined ${roomId}`
-    )
+    try {
+
+      const room = await Room.findById(roomId)
+
+      if (!room) {
+        return socket.emit("error_msg", "Room not found")
+      }
+
+      // ⭐ membership check
+      const isMember = room.members.some(
+        id => id.toString() === socket.user.id
+      )
+
+      if (!isMember) {
+        return socket.emit(
+          "error_msg",
+          "Not authorized for this room"
+        )
+      }
+
+      socket.join(roomId)
+
+      socket.emit("joined_room", roomId)
+
+      console.log(
+        `User ${socket.user.id} joined ${roomId}`
+      )
+
+    } catch (err) {
+      console.error(err)
+    }
+
   })
 
 }
